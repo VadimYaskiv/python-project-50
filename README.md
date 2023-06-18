41881a1f4360f5d6d01014e802a829dec146bebc
Vadim <vadimyaskiv89@gmail.com>
2023-06-18 15:58:57 +0300

    Step 5. Compare flat YAML files

--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 
 ### asciinema. step 3. flat json files comparison
 <a href="https://asciinema.org/a/b1EXOJLv7i7ZoBE4mMWZ29ivQ" target="_blank"><img src="https://asciinema.org/a/b1EXOJLv7i7ZoBE4mMWZ29ivQ.svg" /></a>
+
+### asciinema. step 5. flat yaml files comparison
+<a href="https://asciinema.org/a/FiaOKmKhzkbT7LUwd9mR1G1Pa" target="_blank"><img src="https://asciinema.org/a/FiaOKmKhzkbT7LUwd9mR1G1Pa.svg" /></a>
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -5,7 +5,13 @@ def test_generate_diff():
     with_links_path = 'gendiff/tests/fixtures/rezult.txt'
     with open(with_links_path, 'r') as f:
         rezult = f.read()
+        
         assert generate_diff(
             'gendiff/json_files/file1.json',
             'gendiff/json_files/file2.json'
+        ) == rezult
+
+        assert generate_diff(
+            'gendiff/yaml_files/file1.yaml',
+            'gendiff/yaml_files/file2.yaml'
         ) == rezult
