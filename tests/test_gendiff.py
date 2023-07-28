d0f799fd9f663a0ad8e7cac6b6ad912d6d48346b
Vadim <vadimyaskiv89@gmail.com>
2023-07-28 11:38:22 +0300

    Сorrections to comment №7

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,45 +1,31 @@
+import pytest
 from gendiff.comparison.gendiff_logic import generate_diff
 
 
-def test_generate_diff():
-    links_path = 'tests/fixtures/result_stylish.txt'
-    with open(links_path, 'r') as f:
-        result = f.read()
+test_arguments_values = [('tests/fixtures/json_files/file1.json',
+                          'tests/fixtures/json_files/file2.json', 'stylish',
+                          'tests/fixtures/result_stylish.txt'),
+                         ('tests/fixtures/yaml_files/file1.yaml',
+                          'tests/fixtures/yaml_files/file2.yaml', 'stylish',
+                          'tests/fixtures/result_stylish.txt'),
+                         ('tests/fixtures/json_files/file1.json',
+                          'tests/fixtures/json_files/file2.json', 'plain',
+                          'tests/fixtures/result_plain.txt'),
+                         ('tests/fixtures/yaml_files/file1.yaml',
+                          'tests/fixtures/yaml_files/file2.yaml', 'plain',
+                          'tests/fixtures/result_plain.txt'),
+                         ('tests/fixtures/json_files/file1.json',
+                          'tests/fixtures/json_files/file2.json', 'json',
+                          'tests/fixtures/result_json.txt'),
+                          ('tests/fixtures/yaml_files/file1.yaml',
+                           'tests/fixtures/yaml_files/file2.yaml', 'json',
+                           'tests/fixtures/result_json.txt')]
 
-        assert generate_diff(
-            'tests/fixtures/json_files/file1.json',
-            'tests/fixtures/json_files/file2.json', 'stylish'
-        ) == result
 
-        assert generate_diff(
-            'tests/fixtures/yaml_files/file1.yaml',
-            'tests/fixtures/yaml_files/file2.yaml', 'stylish'
-        ) == result
-
-    links_path = 'tests/fixtures/result_plain.txt'
-    with open(links_path, 'r') as f:
-        result = f.read()
-
-        assert generate_diff(
-            'tests/fixtures/json_files/file1.json',
-            'tests/fixtures/json_files/file2.json', 'plain'
-        ) == result
-
-        assert generate_diff(
-            'tests/fixtures/yaml_files/file1.yaml',
-            'tests/fixtures/yaml_files/file2.yaml', 'plain'
-        ) == result
-
-    links_path = 'tests/fixtures/result_json.txt'
-    with open(links_path, 'r') as f:
-        result = f.read()
-
-        assert generate_diff(
-            'tests/fixtures/json_files/file1.json',
-            'tests/fixtures/json_files/file2.json', 'json'
-        ) == result
-
-        assert generate_diff(
-            'tests/fixtures/yaml_files/file1.yaml',
-            'tests/fixtures/yaml_files/file2.yaml', 'json'
-        ) == result
+@pytest.mark.parametrize("filepath_1, filepath_2, format, result_path", test_arguments_values)
+def test_generate_diff(filepath_1, filepath_2, format, result_path):
+    diff_result = generate_diff(filepath_1, filepath_2, format)
+    with open(result_path, 'r') as f:
+        exp_result = f.read()
+    
+    assert diff_result == exp_result
