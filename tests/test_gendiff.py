c9efd1c60d0cda0e254f2b245c0a10b90cb50fd1
Vadim <vadimyaskiv89@gmail.com>
2023-07-28 11:43:01 +0300

    Сorrections to comment №7_1

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -17,9 +17,9 @@ test_arguments_values = [('tests/fixtures/json_files/file1.json',
                          ('tests/fixtures/json_files/file1.json',
                           'tests/fixtures/json_files/file2.json', 'json',
                           'tests/fixtures/result_json.txt'),
-                          ('tests/fixtures/yaml_files/file1.yaml',
-                           'tests/fixtures/yaml_files/file2.yaml', 'json',
-                           'tests/fixtures/result_json.txt')]
+                         ('tests/fixtures/yaml_files/file1.yaml',
+                          'tests/fixtures/yaml_files/file2.yaml', 'json',
+                          'tests/fixtures/result_json.txt')]
 
 
 @pytest.mark.parametrize("filepath_1, filepath_2, format, result_path", test_arguments_values)
@@ -27,5 +27,5 @@ def test_generate_diff(filepath_1, filepath_2, format, result_path):
     diff_result = generate_diff(filepath_1, filepath_2, format)
     with open(result_path, 'r') as f:
         exp_result = f.read()
-    
+
     assert diff_result == exp_result
